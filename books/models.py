62c1bbba0e2580823c53ab5c70c10b5f62068f3f
Ritesh Malviya <ritesh.riteshmalviya.malviya@gmail.com>
2017-07-15 12:00:23 +0530

    Add book and create migration

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=150)
+  author = models.CharField(max_length=70)
+  review = models.TextField(blank=True, null=True)
+  data_reviewed = models.DateTimeField()
+  is_favourite = models.BooleanField(default=False)
